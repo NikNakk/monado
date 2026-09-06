0af9dcc920d04bce5056f5cf94f4fca53645f996
Nick Kennedy <nick@nick-kennedy.com>
2026-09-06 22:59:53 +0100

    psvr2: implement SLAM timing diagnostics

--- src/xrt/drivers/psvr2/psvr2_prober.c
+++ src/xrt/drivers/psvr2/psvr2_prober.c
@@ -16,7 +16,40 @@
 #include "util/u_misc.h"
 #include "util/u_debug.h"
 
+#include "psvr2.h"
 #include "psvr2_interface.h"
+
+#include <string.h>
+
+
+bool
+psvr2_get_slam_timing(struct xrt_device *xdev, struct psvr2_slam_timing *out)
+{
+	if (xdev == NULL || out == NULL) {
+		return false;
+	}
+
+	*out = (struct psvr2_slam_timing){0};
+
+	/* Avoid treating an arbitrary driver's xrt_device as psvr2_hmd. */
+	if (strstr(xdev->str, "PS VR2") == NULL) {
+		return false;
+	}
+
+	struct psvr2_hmd *hmd = psvr2_hmd(xdev);
+
+	os_mutex_lock(&hmd->data_lock);
+	out->timestamp_samples = hmd->timestamp_samples;
+	out->slam_vts_ns = (int64_t)hmd->last_slam_vts_ns;
+	out->imu_vts_ns = (int64_t)hmd->last_imu_vts_ns;
+	out->hw2mono_vts_ns = (int64_t)hmd->hw2mono_vts;
+	out->slam_monotonic_ns = out->slam_vts_ns + out->hw2mono_vts_ns;
+	out->imu_monotonic_ns = out->imu_vts_ns + out->hw2mono_vts_ns;
+	out->valid = hmd->timestamp_samples >= TIMESTAMP_SAMPLES && hmd->last_slam_vts_ns != 0;
+	os_mutex_unlock(&hmd->data_lock);
+
+	return true;
+}
 
 
 int
