ee4f6263b654fa99b744eb7a6df53a4973850ef7
Nick Kennedy <nick@nick-kennedy.com>
2026-09-07 22:32:45 +0100

    ci: build current Sense integration on macOS
    Use native Apple internal-HID capability detection and compile the constellation, PSVR2 builder, and monado-cli integration in macOS CI.

--- src/xrt/tracking/constellation/t_constellation_tracker_internal.hpp
+++ src/xrt/tracking/constellation/t_constellation_tracker_internal.hpp
@@ -355,7 +355,7 @@ public: // Fields
 	// clang-format off
 	t_constellation_search_model *search_model{nullptr};
 
-	// @todo These need to be pulled from the device and put into the sample. 
+	// @todo These need to be pulled from the device and put into the sample.
 	//       Right now we just hardcode them since we don't have any real sensor fusion.
 	xrt_vec3 prior_pos_error{MIN_POS_ERROR, MIN_POS_ERROR, MIN_POS_ERROR};
 	xrt_vec3 prior_rot_error{MIN_ROT_ERROR, MIN_ROT_ERROR, MIN_ROT_ERROR};
