391f8f83ced627ed64bd3c0872300bc490086674
Nick Kennedy <nick@nick-kennedy.com>
2026-09-08 10:45:13 +0100

    cli: expose PSVR2 calibration recorder

--- src/xrt/targets/cli/cli_common.h
+++ src/xrt/targets/cli/cli_common.h
@@ -34,6 +34,8 @@ cli_cmd_psvr2_pose(int argc, const char **argv);
 int
 cli_cmd_psvr2_camera(int argc, const char **argv);
 int
+cli_cmd_psvr2_calibration_record(int argc, const char **argv);
+int
 cli_cmd_pssense_test(int argc, const char **argv);
 int
 cli_cmd_pose_dump(int argc, const char **argv);
