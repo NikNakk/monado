91df8bc0c374e56080fc1c69ace73db33b6245ec
Nick Kennedy <nick@nick-kennedy.com>
2026-09-20 00:36:56 +0100

    render: write visibility storage descriptor

--- src/xrt/compositor/render/render_compute.c
+++ src/xrt/compositor/render/render_compute.c
@@ -326,6 +326,14 @@ update_compute_shared_descriptor_set(struct vk_bundle *vk,
 	        .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
 	        .pBufferInfo = &buffer_info,
 	    },
+	    {
+	        .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
+	        .dstSet = descriptor_set,
+	        .dstBinding = visibility_binding,
+	        .descriptorCount = 1,
+	        .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,
+	        .pBufferInfo = &visibility_buffer_info,
+	    },
 	};
 
 	vk->vkUpdateDescriptorSets(            //
